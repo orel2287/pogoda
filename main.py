6fb2340ab19d57ff369d571f01df6bfaa053bf1d
orel2287 <147095559+orel2287@users.noreply.github.com>
2024-09-16 23:27:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
+from flask import Flask, request, jsonify
 import requests
-from flask import Flask, render_template, request, jsonify
-
-API_KEY = '47d89e1c6e7a4d908f47327c49d7bfeb'
-BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=' + API_KEY
-
 app = Flask(__name__)
 
-def get_weather(city):
-    url = BASE_URL.format(city=city)
-    response = requests.get(url)
-    data = response.json()
+API_KEY = '47d89e1c6e7a4d908f47327c49d7bfeb'  # Замените на ваш API ключ OpenWeatherMap
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
-    # Форматируем данные для вывода
-    weather_info = {
-        'city': city,
-        'temperature': round(data['main']['temp'] - 273.15, 2),  # Перевод в Цельсий
-        'description': data['weather'][0]['description'],
-        'humidity': data['main']['humidity'],
-        'pressure': data['main']['pressure'],
-        'wind_speed': data['wind']['speed'],
-        'icon': f"http://openweathermap.org/img/wn/{data['weather'][0]['icon']}@2x.png"
-    }
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')  # Получаем название города из параметров запроса
+    if not city:
+        return jsonify({"error": "Please provide a city name."}), 400
 
-    return weather_info
+    response = requests.get(BASE_URL, params={
+        'q': city,
+        'appid': API_KEY,
+        'units': 'metric'  # Для получения температуры в Цельсиях
+    })
 
-@app.route('/weather/<city>')
-def weather(city):
-    weather_data = get_weather(city)
-    return jsonify(weather_data)
+    if response.status_code == 200:
+        data = response.json()
+        weather_info = {
+            "city": data['name'],
+            "temperature": data['main']['temp'],
+            "description": data['weather'][0]['description'],
+            "humidity": data['main']['humidity']
+        }
+        return jsonify(weather_info)
+    else:
+        return jsonify({"error": "City not found."}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
